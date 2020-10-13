f8a81dec33ad37d60b1ca4837701f4aa2b549790
sergyby <serg7@tut.by>
2020-10-13 09:48:47 +0300

    login\logout

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, session, redirect
 import sqlite3
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -41,6 +41,45 @@ def signUp():
         return json.dumps({'html': '<span>Должны быть заполнены все поля</span>'}, ensure_ascii=False)
 
 
+@app.route('/showSignin')
+def showSignin():
+    return render_template('signin.html')
+
+
+@app.route('/validateLogin', methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        query = cursor.execute("select * from tbl_user where user_username= ? ", (_username,)).fetchone()
+        if not query is None:
+            if check_password_hash(str(query[3]), _password):
+                session['user'] = query[1]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html', error='Wrong Email address or Password.')
+        else:
+            return render_template('error.html', error='Wrong Email address or Password.')
+
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html', error='Unauthorized Access')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
+
 def create_db():
     query = cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='tbl_user'").fetchone()
     if (not query is None) and (query[0] == 0):
@@ -54,4 +93,5 @@ def create_db():
 
 if __name__ == "__main__":
     if create_db():
+        app.secret_key = b'_5#y2L"HGF8z&%^Tf]/'
         app.run()
